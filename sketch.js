a2c99600f65ec82b9c7accbd635a24c2fd724574
Samanimkr <samanimkr@outlook.com>
2019-07-09 15:05:23 +0100

    Colour selection functionality

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
-let brushColor = 0;
-let brushSize = 16;
+const OPTIONS_MENU_HEIGHT = 88;
+let brushColor = 'black';
+let brushSize = 10;
+
 
 function setup() {
     const screenWidth = window.innerWidth || document.body.clientWidth;
-    createCanvas(screenWidth, 480);
+    const screenHeight = window.innerHeight || document.body.clientHeight;
+    createCanvas(screenWidth, screenHeight - OPTIONS_MENU_HEIGHT);
 }
 
 function draw() {
@@ -12,5 +15,19 @@ function draw() {
 
     if (mouseIsPressed) {
         line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+}
+
+const $colors = document.getElementsByClassName('color');
+for (let color of $colors) color.addEventListener('click', handleColorChange);
+
+function handleColorChange() {
+    const isSelected = this.classList.contains('selected');
+    if (!isSelected) {
+        const prevColor = document.getElementsByClassName(brushColor)[0];
+        prevColor.classList.remove('selected');
+        this.classList.add('selected');
+
+        brushColor = this.classList[1];
     }
 }
