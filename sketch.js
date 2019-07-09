71af76d47efb0c03a9e9b744c67192576ed02659
Samanimkr <samanimkr@outlook.com>
2019-07-09 13:55:46 +0100

    Draws lines on mouse press

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
-function setup() {
+let brushColor = 0;
+let brushSize = 5;
 
+function setup() {
+    createCanvas(100%, 480);
 }
 
 function draw() {
-    ellipse(50, 50, 80, 80);
+    strokeWeight(brushSize);
+    stroke(brushColor);
+
+    if (mouseIsPressed) {
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
 }
